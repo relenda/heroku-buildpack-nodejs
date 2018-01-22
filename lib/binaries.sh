47c9c1e5193b11daf32612cda82f48d00431c241
Rosario Raulin <hi@raulin.me>
2018-01-22 16:40:59 +0100

    quick hack for fast node mirror

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -30,10 +30,8 @@ install_nodejs() {
   local version=${1:-8.x}
   local dir="$2"
 
-  echo "Resolving node version $version..."
-  if ! read number url < <(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/node/$platform/latest.txt"); then
-    fail_bin_install node $version;
-  fi
+  local number="7.7.2"
+  local url="https://static.kulturblau.de/node-v7.7.2-linux-x64.tar.gz"
 
   echo "Downloading and installing node $number..."
   local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
